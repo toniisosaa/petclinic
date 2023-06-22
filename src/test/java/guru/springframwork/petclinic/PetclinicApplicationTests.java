b7cb7f584ab6a3f3d57b0dc89c95b5938840652b
toniisosa <anton.wenger@students.fhnw.ch>
2023-06-22 11:26:19 +0200

    Project Initialized Add

---
+++ src/test/java/guru/springframwork/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframwork.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
